c328a0fa8740872c241b30359c1f3d73a48ad4ad
Nixon Cruz <nixonbcruzh@gmail.com>
2025-09-27 13:45:23 +0200

    Upload yowini

---
+++ Pruebas_con_yowiny/src/ejercicio_1/Main.java
@@ -0,0 +1,27 @@
+package ejercicio_1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Dime una opcionion");
+		int opcion= sc.nextInt();
+		switch (opcion) {
+		case 1:
+			System.out.println("Hola");
+			System.out.print("hola");
+			System.out.println("Hola");
+			
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
--- Tema2/src/boletin6/Boletin6.java
+++ Tema2/src/boletin6/Boletin6.java
@@ -38,5 +38,7 @@ public class Boletin6 {
 		
 		
 	        }
+	        
+	      
 	    }
 	    }
